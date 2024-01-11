7e2e13e11ddd5ad06157edb1a990e67f99b73f8b
Nikobird <119409364+Nikobird@users.noreply.github.com>
2024-01-11 13:05:22 +0000

    start

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'RPA',
+  serviceName: 'Risk protection arrangement',
 
   // Default port that prototype runs on
   port: '3000',
